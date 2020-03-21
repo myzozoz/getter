2458497c21c09bd6402434b31963129bfab0d4e2
mholopainen <markus.holopainen@avarko.com>
2020-03-21 14:54:19 +0200

    Added the script itself and wrote out README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # getter
-Shell script to retrieve test files used for Programming for Performance assignment 1
+Shell script to retrieve test files used for Programming for Performance Assignment 1
+
+The script downloads files given as parameters. Files are placed inside a new directory (named `data` by default) in the current working directory.
+
+### Usage
+1. Download or copy+paste the contents of `getter.sh`
+2. Make sure the file has execution rights (`chmod u+x <possible_path>getter.sh`)
+3. Run like any other executable with the file numbers you want to download as parameters (e.g. `<possible_path>/getter.sh 0 1 2 3` downloads files F0, F1, F2 and F3)
+
+### Parameters
+- accepts any number of integers as parameters
+- `-c` or `--clear` deletes the current data folder
+- `-a` or `--all` fetches all of the files
+⋅⋅* Downloading all of the files will take some time and some space, so use at your own risk
+
+### Options
+You can change a few things if you edit the script
+- `LAST_FILE` is the number of the last file on the server (245 at the time of writing, this could theoretically change)
+- `DATA_DIR` is the name of the directory, where downloaded files are placed (`data` by default)
+- `CURL_OPTIONS` can be used if you want to set run parameters for the curl commands (for example `-s`to run curl in silent mode)
+- `FILE_PREFIX` is the prefix used for naming the downloaded files (`F` by default)
+- `BASE_URL` is pretty obvious, but might change at some point. Who knows?
+
+### Other
+You might want to set an alias for the command for more comfortable useage (mine is set to `alias gt="<path_to_file/getter.sh"`>) for reusability.
---
+++ getter.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+LAST_FILE=245
+DATA_DIR="data"
+CURL_OPTIONS=""
+FILE_PREFIX="F"
+BASE_URL="https://www.cs.helsinki.fi/u/puglisi/PFP2020/assignment1/F"
+
+# Create directory named 'data' in current working directory (if it doesn't exist)
+if [[ ! -d $DATA_DIR ]]
+then
+    mkdir $DATA_DIR
+    echo "Created folder '$DATA_DIR' in current working directory"
+fi
+
+download_file()
+{
+    ARG=$1
+    if [ $ARG -gt $LAST_FILE -o $ARG -lt 0 ]
+    then
+        echo "File $FILE_PREFIX$ARG does not exist on server"
+    elif [[ -f "data/$FILE_PREFIX$ARG" ]]
+    then
+        echo "File $FILE_PREFIX$ARG already downloaded"
+    else
+        curl $CURL_OPTIONS "$BASE_URL$ARG" > "data/$FILE_PREFIX$ARG"
+        echo "Downloaded file $FILE_PREFIX$ARG"
+    fi
+}
+
+download_all_files()
+{
+    echo "Downloading all files..."
+    for (( i=0; i<=$LAST_FILE; i++ ))
+    do
+        download_file $i
+    done
+}
+
+
+for ARG in "$@"
+do
+    # Clear command
+    if [ "$ARG" = "-c" -o "$ARG" = "--clear" ]
+    then
+        echo "Clearing all downloaded files..."
+        rm -rd data
+        break
+    elif [ "$ARG" = "-a" -o "$ARG" = "--all" ]
+    then
+        while true
+        do
+            read -p "Are you sure you want to download all of the files? This will take some time and space... [Y/N]" yn
+            case $yn in
+                [Yy]* ) download_all_files; break;;
+                [Nn]* ) break;;
+            esac
+        done
+
+    # Otherwise we only accept numbers
+    elif ! [[ $ARG =~ ^[0-9]+$ ]]
+    then
+        echo "Argument '$ARG' not a valid number!"
+        continue
+    else
+        #If not stopped yet, we will download the file
+        download_file $ARG
+    fi
+
+done
